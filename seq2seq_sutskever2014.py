5fc1cedaafcfcfbc9f5c4545749a0ccd51d95458
Martins Frolovs <martins.frolovs@gmail.com>
2019-08-09 00:28:33 +0300

    epochs

--- seq2seq_sutskever2014.py
+++ seq2seq_sutskever2014.py
@@ -7,11 +7,12 @@ from fra_eng_dataset import FraEngDataset, fra_eng_dataset_collate
 from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence, pad_packed_sequence
 
 RNN_LAYERS = 4
-RNN_HIDDEN_SIZE = 128
+RNN_HIDDEN_SIZE = 1024
 IN_EMBEDDING_SIZE = 128
 OUT_EMBEDDING_SIZE = 128
-BATCH_SIZE = 128
+BATCH_SIZE = 64
 MAX_OUTP_TIMESTEPS = 20
+EPOCHS = 50
 
 device = 'cpu'
 
@@ -118,41 +119,45 @@ rnn_decoder = RNN_decoder_model(dataset.get_fra_dict_size()).to(device)
 
 
 params = list(rnn_encoder.parameters()) + list(rnn_decoder.parameters())
-optimizer = torch.optim.Adam(params, lr = 1e-4)
+optimizer = torch.optim.Adam(params, lr = 1e-3)
 
-for idx, sentences in enumerate(sentences_loader):
+for epoch in range(EPOCHS):
 
-   rnn_encoder.init_hidden_and_cell()
-   
-   eng_sentences = sentences['eng_sentences']
-   eng_lens = sentences['eng_lens']
-   fra_sentences = sentences['fra_sentences']
-   fra_lens = sentences['fra_lens']
+    print(f"Starting epoch {epoch} =====================")
 
-   padded_eng = pad_sequence(eng_sentences, padding_value=0).to(device)
-   padded_fra = pad_sequence(fra_sentences, padding_value=0).to(device)
+    for idx, sentences in enumerate(sentences_loader):
 
-   padded_eng_one_hot = torch.zeros(padded_eng.shape[0], padded_eng.shape[1], dataset.get_eng_dict_size()).to(device)
-   padded_eng_one_hot = padded_eng_one_hot.scatter_(2,padded_eng.data,1)
-   
-   rnn_encoder.forward((padded_eng_one_hot, eng_lens))
-   hidden, cell = rnn_encoder.get_hidden_and_cell()
-   
-   rnn_decoder.init_hidden_and_cell(hidden,cell)
-   
-   max_sentence_len = padded_fra.shape[0]
-   y_pred = rnn_decoder.forward(dataset.get_fra_eos_code(), dataset.get_fra_dict_size(), max_sentence_len)
+       rnn_encoder.init_hidden_and_cell()
+       
+       eng_sentences = sentences['eng_sentences']
+       eng_lens = sentences['eng_lens']
+       fra_sentences = sentences['fra_sentences']
+       fra_lens = sentences['fra_lens']
+
+       padded_eng = pad_sequence(eng_sentences, padding_value=0).to(device)
+       padded_fra = pad_sequence(fra_sentences, padding_value=0).to(device)
+
+       padded_eng_one_hot = torch.zeros(padded_eng.shape[0], padded_eng.shape[1], dataset.get_eng_dict_size()).to(device)
+       padded_eng_one_hot = padded_eng_one_hot.scatter_(2,padded_eng.data,1)
+       
+       rnn_encoder.forward((padded_eng_one_hot, eng_lens))
+       hidden, cell = rnn_encoder.get_hidden_and_cell()
+       
+       rnn_decoder.init_hidden_and_cell(hidden,cell)
+       
+       max_sentence_len = padded_fra.shape[0]
+       y_pred = rnn_decoder.forward(dataset.get_fra_eos_code(), dataset.get_fra_dict_size(), max_sentence_len)
 
 
-   padded_fra_one_hot = torch.zeros(padded_fra.shape[0], padded_fra.shape[1], dataset.get_fra_dict_size()).to(device)
-   padded_fra_one_hot = padded_fra_one_hot.scatter_(2,padded_fra.data,1)
-   
-   #Make all padded one-hot vectors to all zeros, which which will make
-   #padded components loss 0 and sop wont affect the loss
-   padded_fra_one_hot[:,:,0] = torch.zeros(padded_fra_one_hot.shape[0], padded_fra_one_hot.shape[1])
-   loss = torch.sum(-torch.log(y_pred + 1e-9) * padded_fra_one_hot)
-   print(loss.to('cpu').detach().data)
-   
-   loss.backward()
-   optimizer.step()
-   optimizer.zero_grad()
+       padded_fra_one_hot = torch.zeros(padded_fra.shape[0], padded_fra.shape[1], dataset.get_fra_dict_size()).to(device)
+       padded_fra_one_hot = padded_fra_one_hot.scatter_(2,padded_fra.data,1)
+       
+       #Make all padded one-hot vectors to all zeros, which which will make
+       #padded components loss 0 and sop wont affect the loss
+       padded_fra_one_hot[:,:,0] = torch.zeros(padded_fra_one_hot.shape[0], padded_fra_one_hot.shape[1])
+       loss = torch.sum(-torch.log(y_pred + 1e-9) * padded_fra_one_hot)
+       print(loss.to('cpu').detach().data)
+       
+       loss.backward()
+       optimizer.step()
+       optimizer.zero_grad()
